6e9868599ce2a143d31415bd26092ed0693b8a2b
Douglas Hagan <dhagan@yahoo.com>
2014-12-22 22:40:00 -1000

    add markers and polylines to demo5

--- app/js/demo5.js
+++ app/js/demo5.js
@@ -1,49 +1,98 @@
 /**
  * Created by dhagan on 12/22/2014.
  */
-var flightPath;
+
 var map;
+var polylines = [];
+
 function initialize() {
 
-    var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+    var center = new google.maps.LatLng(30.35, 47.21);
 
 
     var mapOptions = {
-        zoom: 3,
-        center: new google.maps.LatLng(0, -180),
+        zoom: 8,
+        center: center,
         mapTypeId: google.maps.MapTypeId.TERRAIN
     };
 
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
+    var markerArray = [
+        {
+            position : new  google.maps.LatLng(30.35, 47.21),
+            icon: 'img/red-cross.png'
+        },
+        {
+            position : new  google.maps.LatLng(30.46,45.90),
+            icon: 'img/red-cross.png'
+        },
+        {
+            position : new  google.maps.LatLng(30.12, 47.98),
+            icon: 'img/red-cross.png'
+        },
+        {
+            position : new  google.maps.LatLng(29.37, 48.39),
+            icon: 'img/red-cross.png'
+        }
+        ]
 
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map,
-        title: 'Hello World!'
-    });
+    var markers = [];
 
-    var flightPlanCoordinates = [
-        new google.maps.LatLng(37.772323, -122.214897),
-        new google.maps.LatLng(21.291982, -157.821856),
-        new google.maps.LatLng(-18.142599, 178.431),
-        new google.maps.LatLng(-27.46758, 153.027892)
-    ];
+    var circleSymbol = {
+        path        : google.maps.SymbolPath.CIRCLE,
+        scale       : 8,
+        strokeColor : '#009ee0'
+    };
 
-    flightPath = new google.maps.Polyline({
-        path: flightPlanCoordinates,
-        strokeColor: '#FF0000',
-        strokeOpacity: 1.0,
-        strokeWeight: 2
-    });
+    for (var i = 0; i < markerArray.length; i++) {
+        var marker = new google.maps.Marker({
+            position: markerArray[i].position,
+            map: map,
+            icon: markerArray[i].icon
+            //title: 'Hello World!'
+        });
+        markers.push(marker);
+
+        if (i > 0) {
+            var polyline = new google.maps.Polyline( {
+                path          : [
+                    markerArray[i-1].position,
+                    markerArray[i].position
+                ],
+                icons         : [ {
+                    icon   : circleSymbol,
+                    offset : '0%'
+                } ],
+                clickable     : false,
+                geodesic      : true,
+                strokeColor   : 'black',
+                strokeOpacity : .6,
+                strokeWeight  :  3,
+                map           : map
+            } );
+            polylines.push(polyline);
+        }
+    }
+
+    var counter = 0;
+
+    window.setInterval( function() {
+        var icons = polylines[0].get( 'icons' );
+
+        counter = ( counter +1 ) % 200;
+
+        icons[0].offset = ( counter /2 ) + '%';
+        polylines[0].set( 'icons', icons );
+    }, 20 );
 
     addLine();
 }
 
 function addLine() {
-    flightPath.setMap(map);
+    polylines[0].setMap(map);
 }
 
 function removeLine() {
-    flightPath.setMap(null);
+    polylines[0].setMap(null);
 }
